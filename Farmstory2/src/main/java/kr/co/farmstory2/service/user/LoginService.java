a1dff2dc3dfbae2076e62514962fab8d75157ad5
a3539a <a3539a@gmail.com>
2021-07-08 11:17:47 +0900

    2021/07/08 JSP 로그인,로그아웃 구현완료

--- Farmstory2/src/main/java/kr/co/farmstory2/service/user/LoginService.java
+++ Farmstory2/src/main/java/kr/co/farmstory2/service/user/LoginService.java
@@ -2,14 +2,44 @@ package kr.co.farmstory2.service.user;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import kr.co.farmstory2.dao.MemberDao;
 import kr.co.farmstory2.service.CommonService;
+import kr.co.farmstory2.vo.MemberVo;
 
 public class LoginService implements CommonService{
 
 	@Override
 	public String requestProc(HttpServletRequest req, HttpServletResponse resp) {
-		return "/user/login.jsp";
+		
+		if(req.getMethod().equals("GET")) {
+			// GET
+			return "/user/login.jsp";
+
+		}else {
+			// POST
+			String uid = req.getParameter("uid");
+			String pass = req.getParameter("pass");
+			
+			MemberVo mv = MemberDao.getInstance().selectMember(uid, pass);
+			
+			if(mv != null) {
+				// 회원 맞음
+				
+				HttpSession sess = req.getSession();
+				sess.setAttribute("sessMember", mv);
+				
+				return "redirect:/Farmstory2/";
+
+			}else {
+				// 회원 아님
+				return "redirect:/Farmstory2/user/login.do?success=100";
+			}
+			
+
+		}
+
 	}
 
 }
--- Farmstory2/src/main/webapp/user/register.jsp
+++ Farmstory2/src/main/webapp/user/register.jsp
@@ -1,5 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="../_header.jsp" %>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="/Farmstory2/js/checkUid.js"></script>
+
 <section id="user" class="register">
     <form action="/Farmstory2/user/register.do" method="POST">
         <table border="1">
